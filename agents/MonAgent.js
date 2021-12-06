b5505f5b538e8cf93128b7905300ab1cb4b7e1c3
Xavier <xavier.vicente77@gmail.com>
2021-12-06 22:32:06 +0100

    un autre

--- agents/MonAgent.js
+++ agents/MonAgent.js
@@ -19,56 +19,78 @@ export default SocketAgent(function () {
 	
 	let x = 0;
 	let	y = 0;
-	var marray = [];
-	var map;
-	var cellx;
+	let marray = [];
+	let map=[];
 	var celly;
+	var cellx;
 	// fonction qui contient la logique de l'agent
 	return function (sendMessage, messages) {
 		//debut de l'action de l'agent
-
 		//console.log(parseMessage(messages))
 		marray = messages.map(parseMessage)[0].data
-
 
 		//console.log(marray)
 		for (var element in marray) {
 			//console.log("elem1 " + element)
 			for (var element2 in marray[element]) {
-				console.log(marray[element][element2]);
-				switch (element) {
+				//console.log(marray[element][element2]);
+				switch (parseInt(element)) {
 					case 0 :
-						celly = y - 1;
+						var celly = y - 1;
+						break;
 					case 1 :
-						celly = y;
+						var celly = y;
+						break;
 					case 2:
-						celly = y + 1;
+						var celly = y + 1;
+						break;
+					default:
+						break;
 				}
-				console.log("elemnent2: "+element2)
-				switch (element2) {
+				//console.log("elemnent2: " + element2)
+				switch (parseInt(element2)) {
 					case 0:
 						var cellx = x - 1;
-						console.log("elelment 2 = 0 ",cellx);
+						//console.log("elelment 2 = 0 ",cellx);
+						break;
 					case 1:
-						console.log("elelment 2 = 1 ", cellx);
 						var cellx = x;
+						//console.log("elelment 2 = 1 ", cellx);
+						break;
 					case 2:
-						console.log("elelment 2 = 2 ", cellx);
 						var cellx = x + 1;
+						//console.log("elelment 2 = 2 ", cellx);
+						break;
+					default:
+						break;
 				}
-				console.log("Coordoné x y cellule " + cellx + " " + celly)
+
+				var cell = {
+					"Type": marray[element][element2].type,
+					"x": cellx,
+					"y": celly
+				}
+				//if (map.indexOf(cell) == -1) {
+				//	map.push(cell)	
+				//}
+				if (map.length == 0) {
+					map.push(cell);
+				}
+				var bool1 = false;
+				for (var i = 0; i < map.length; i++) {
+					if (map[i] == cell) {
+						bool1 = true;
+					}
+				}
+				if (bool1 == true) {
+					map.push(cell);
+                }
+
+
 			}
 		}
-		//for (var i = 0; i < marray.length; i++) {
-		//	for (var y = 0; y < marray[i].length; y++) {
-		//		console.log(i + " " + y +marray[i][y]);
-  //          }
-
-  //      }
-
-
+		console.log(map)
 		console.log("########################################")
-
 		sendMessage("context");
 
 		// fin de l'action l'agent
